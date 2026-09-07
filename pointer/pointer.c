526d1908fa8190f6d306758d54917c41cd4ba2e7
Sid-Tek <sidtekumalla@gmail.com>
2026-09-07 12:35:59 -0400

    Part 1 Code Edit

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -17,14 +17,23 @@ int main(void)
     int a[4] = {10, 20, 30, 40};
     int *p = a;
     int **pp = &p;
+    int i;
 
     struct pair local = {1, 2};
     struct pair *heap = malloc(sizeof *heap);
+    struct pair *qwe = malloc(100 * sizeof *qwe);
     if (heap == NULL)
         return 1;
+    if (qwe == NULL)
+        return 2;
 
     heap->x = 5;
     heap->y = 6;
+
+    for (int i=0; i<100; i++){
+        (qwe+i)->x = i;
+        (qwe+i)->y = 2*i;
+    }
 
     update(&a[1], heap);
 
@@ -35,5 +44,6 @@ int main(void)
        objects, initialize element i to {i, 2*i}, and free it. */
 
     free(heap);
+    free(qwe);
     return 0;
 }
